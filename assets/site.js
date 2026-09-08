12e49359589059d32cc41923a5d6d6687772f999
hosseinayazi <hossein.ayazi@gmail.com>
2026-09-08 16:59:25 -0700

    Add site navigation script

---
+++ assets/site.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.menu-button');
+const nav = document.querySelector('.main-nav');
+if (button && nav) {
+  button.addEventListener('click', () => {
+    const open = nav.classList.toggle('open');
+    button.setAttribute('aria-expanded', open ? 'true' : 'false');
+  });
+}
